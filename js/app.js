abd98768cf6602666a6e4da687035d31b8d5d5df
Gideon Isa <innocentgideon10@gmail.com>
2021-07-29 09:15:05 +0100

    initial commit

---
+++ js/app.js
@@ -0,0 +1,58 @@
+let hublot = document.querySelector("#hublot");
+let race = document.querySelector("#race");
+let onchesil = document.querySelector("#on-chesil");
+let chinatown = document.querySelector("#chinatown");
+let ladyBird = document.querySelector("#ladybird");
+let moonLight = document.querySelector("#moonlight");
+let firstReform = document.querySelector("#firstReformed");
+let century = document.querySelector("#century");
+
+hublot.addEventListener("click", displayhublot);
+race.addEventListener("click", displayrace);
+onchesil.addEventListener("click", displayonChesil);
+chinaTown.addEventListener("click", displaychina);
+ladyBird.addEventListener("click", displayladyBird);
+moonLight.addEventListener("click", displaymoonLight);
+moonfirstReform.addEventListener("click", displayfirstReformed);
+century.addEventListener("click", displaycentury);
+
+function displayhublot() {
+    
+    window.open("hublot.html", "Kanopy Movies", "Width=400", "height=600 scrollbars=yes")
+
+}
+function displayrace() {
+    
+    window.open("race.html", "Kanopy Movies", "Width=400", "height=600 scrollbars=yes")
+
+}
+function displayonChesil() {
+    
+    window.open("onchesil.html", "Kanopy Movies", "Width=400", "height=600 scrollbars=yes")
+
+}
+function displaychina() {
+    
+    window.open("chinaTown.html", "Kanopy Movies", "Width=400", "height=600 scrollbars=yes")
+
+}
+function displayladyBird() {
+    
+    window.open("ladyBird.html", "Kanopy Movies", "Width=400", "height=600 scrollbars=yes")
+
+}
+function displaymoonLight() {
+    
+    window.open("moonLight.html", "Kanopy Movies", "Width=400", "height=600 scrollbars=yes")
+
+}
+function displayfirstReformed() {
+    
+    window.open("firstReformed.html", "Kanopy Movies", "Width=400", "height=600 scrollbars=yes")
+
+}
+function displaycentury() {
+    
+    window.open("century.html", "Kanopy Movies", "Width=400", "height=600 scrollbars=yes")
+
+}
